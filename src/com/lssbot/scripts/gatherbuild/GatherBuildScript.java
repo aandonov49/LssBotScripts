2d1f04295a75c2ecc42fd2e944e2bfbde57fdfb8
Atanas Andonov <aandonov@ad4u-bg.com>
2024-04-28 15:09:04 +0300

    Added Open Build menu on the map

--- src/com/lssbot/scripts/gatherbuild/GatherBuildScript.java
+++ src/com/lssbot/scripts/gatherbuild/GatherBuildScript.java
@@ -3,21 +3,22 @@ package com.lssbot.scripts.gatherbuild;
 import com.lssbot.core.api.device.Device;
 import com.lssbot.core.api.game.Game;
 import com.lssbot.core.api.game.roe.menus.build.ROEBuildMenuCategory;
+import com.lssbot.core.api.geometry.BRectangle;
 import com.lssbot.core.api.script.AbstractScript;
 import com.lssbot.core.api.script.ScriptManifest;
 import com.lssbot.core.api.script.config.Config;
 
 @ScriptManifest(
-        game= Game.RISE_OF_EMPIRES,
-        name= "Gather Build",
+        game = Game.RISE_OF_EMPIRES,
+        name = "Gather Build",
         version = 0.0,
         timeoutMinutes = 10,
         description = "Build rss rss tile with specific coordinates and send Legion to gather on it.",
-        author= "Atanas Andonov"
+        author = "Atanas Andonov"
 )
 public class GatherBuildScript extends AbstractScript {
     private final GatherBuildConfig CONFIG = new GatherBuildConfig();
-    private boolean scrollRight=false;
+    private boolean scrollRight = false;
 
     public GatherBuildScript(Device device) {
         super(device);
@@ -35,8 +36,8 @@ public class GatherBuildScript extends AbstractScript {
 
     @Override
     public void onStart() {
-        if (CONFIG.gather.getButtons().contains(ROEBuildMenuCategory.IRON.name())){
-            scrollRight=true;
+        if (CONFIG.gather.getButtons().contains(ROEBuildMenuCategory.IRON.name())) {
+            scrollRight = true;
         }
         super.onStart();
     }
@@ -48,14 +49,37 @@ public class GatherBuildScript extends AbstractScript {
 
     @Override
     public int loop() {
-        if (!roe().getViewport().isOnMap()){
-            roe().getViewport().openMap();
+        if (!isOnMap()) {
+            return -1;
         }
+        openMapBuildMenu();
         return 0;
     }
 
     @Override
     public Config getConfig() {
         return CONFIG;
+    }
+
+    private boolean isOnMap() {
+        if (!roe().getViewport().isOnMap()) {
+            roe().getViewport().openMap();
+            return roe().getViewport().isOnMap();
+        }
+        return false;
+    }
+
+    private void openMapBuildMenu() {
+        if (!isBuidlMenuOpen()) {
+            device.log("Open Build Menu on the map ");
+            roe().getViewport().openBuildMenu();
+            isBuidlMenuOpen();
+        }
+    }
+
+    private boolean isBuidlMenuOpen() {
+        BRectangle buildMenuText = getOpenCV().getAreaMatch(Images.BUILD_MENU_TEXT, 0.75);
+        device.log("Is Build Menu on the map Open? " + (buildMenuText == null ? "False" : "True"));
+        return buildMenuText != null;
     }
 }
---
+++ src/com/lssbot/scripts/gatherbuild/Images.java
@@ -0,0 +1,10 @@
+package com.lssbot.scripts.gatherbuild;
+
+import java.io.File;
+
+public class Images {
+    private static String PREFIX = "roe" + File.separator + "gatherbuild" + File.separator;
+    public static final String BUILD_MENU_TEXT = PREFIX + "build";
+    public static final String BUTTON_OPEN_FORMATION_SETTINGS = PREFIX + "settings";
+    public static final String BUTTON_QUICK_SETTING = PREFIX + "quick";
+}
